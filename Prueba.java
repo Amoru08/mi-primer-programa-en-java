981c79de2189d89786b9a03965393e37b1dfb4b1
Amor <amore.acecalrgmail.com>
2022-10-10 09:01:13 +0200

    Añadimos la edad y la fecha de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,7 @@
 public class Prueba{
         public static void main(String args[]){
             System.out.println("Acebes Calvo");
+            System.out.println("18 anos");
+            System.out.println("06/10/2004");
         }
 }
