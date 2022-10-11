1366fe4ce93fd4f893cb7210e299c83bce42de5b
Amor <amore.acecalrgmail.com>
2022-10-11 08:54:24 +0200

    Ponemos el nombre de nuestro artista favorito

--- Prueba.java
+++ Prueba.java
@@ -1,8 +1,8 @@
 public class Prueba{
         public static void main(String args[]){
-            System.out.println("Acebes Calvo");
             System.out.println("18 anos");
             System.out.println("06/10/2004");
             System.out.println("Pizza");
+            System.out.println("The 1975");
         }
 }
